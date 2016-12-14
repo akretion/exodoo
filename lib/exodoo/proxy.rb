f384529e5b8595b30624421f1c7ec14fafc06479
Raphaël Valyi <rvalyi@akretion.com>
2016-12-14 02:04:26 -0200

    fixed proxy, use the new ooor login_if_required session method

--- lib/exodoo/proxy.rb
+++ lib/exodoo/proxy.rb
@@ -1,6 +1,8 @@
 require 'rack/reverse_proxy'
 require 'nokogiri'
 require 'rack'
+require 'uri'
+require 'exodoo/clipping'
 
 
 module Rack
@@ -15,20 +17,27 @@ end
 module RackReverseProxy
   class RoundTrip
 
+    include Exodoo::Clipping
+
     def rack_response
-      body = target_response.body
-      place_holder = Net::HTTP.get_response(URI("http://localhost:3333/proxy")) # TODO dynamic
-      doc = Nokogiri::XML(body.to_s)
-      main = doc.xpath("//main")[0].to_s
-      body2 = place_holder.body.gsub('mainnn', main)
-      [target_response.status, response_headers, [body2]]
+      body = target_response.body.to_s # NOTE this kills the streaming feature, how much do we care for Odoo?
+
+      if response_headers['Content-Type'] =~ /text\/html/
+        body = apply_site_layout(body, env, "//main")
+      end
+      if target_response.status == 302
+        hacked_redirect = URI.parse(response_headers['Location'])
+        hacked_redirect.port = source_request.port
+        response_headers['Location'] = hacked_redirect.to_s
+      end
+      [target_response.status, response_headers, [body]]
     end
 
     # Make sure proxied requests will include Odoo session_id so it will pass the CSRF token check
     def initialize_http_header
-      target_request_headers['Cookie'] = "session_id=#{env['ooor']['ooor_session'].web_session[:session_id]}"
-      p "PROXYYYYYYYYYYY", env['ooor']['ooor_session'].web_session[:session_id]
-      p "PROXYYYYYYY2222", env['ooor']['ooor_session']
+      session = env['ooor']['ooor_session']
+      session.login_if_required()
+      target_request_headers['Cookie'] = "session_id=#{session.web_session[:session_id]}"
       target_request.initialize_http_header(target_request_headers)
     end
 
