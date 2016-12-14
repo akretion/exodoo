7b6055885e697bc156ba0c5e950154887f8d69bc
Raphaël Valyi <rvalyi@akretion.com>
2016-12-14 02:05:08 -0200

    cart fixed for adding products and changing their quantity. redirect to Odoo proxied checkout

--- lib/exodoo/shopify/cart.rb
+++ lib/exodoo/shopify/cart.rb
@@ -8,20 +8,39 @@ require 'rack/reverse_proxy'
 
 class Exodoo::Cart
   attr_accessor_initialize :app
-  def call(env)
-    if env['PATH_INFO'] == "/cart/add.js"
-      session = env['ooor']['ooor_public_session'] || env['ooor']['ooor_session']
-      req = Rack::Request.new(env)
-      product = session.const_get('product.product').find(req.params['id'])
-      qty = req.params['quantity'].to_f
-      p product.attributes.keys
-      p product.associations.keys
-      p product
-      p product.associations
+
+  def cart_to_cartjs(env)
+    session = env['ooor']['ooor_session']
+    order_id = session.const_get('website').find(:first).sale_get_order_rpc(true, nil, false, false, {}) # TODO
+    order = session.const_get('sale.order').find(order_id)
+    cartjs = {'items' => []}
+    order.order_line.each do |line|
+      product = line.product_id
+      qty = line.product_uom_qty
+      cartjs['items'] << item_to_cartjs(product, qty)
+    end
+
+    cartjs.merge!({
+        token: "TODO",
+        note: "",
+        attributes: {},
+        original_total_price: 42,
+        total_discount: 0,
+        total_weight: 0,
+      })
+    cartjs.stringify_keys!
+    cartjs['item_count'] = cartjs['items'].size
+#    cartjs['total_price'] = cartjs['items'].map{|item| item[:price]}.inject(0){|sum, x| sum + x }
+      #      session['cart']['total_discount'] = ...
+      #      session['cart']['total_weight'] = ...
+    cartjs
+  end
+
+  def item_to_cartjs(product, qty)
       handle = CGI.escape(product.to_param.gsub(' ', '-')),
-      data = {id: product.id, # TODO tpl?
+      item = {id: product.id, # TODO tpl?
               properties: nil, # TODO
-              quantity: req.params['quantity'],
+              quantity: qty,
               variant_id: product.id,
               key: "TODO",
               title: product.display_name,
@@ -45,67 +64,42 @@ class Exodoo::Cart
               requires_shipping: true
       }
       if product.attribute_value_ids
-        p product.attribute_value_ids
-        data['variant_title'] = (product.attribute_value_ids.map {|i| "#{i.name}"}).join(" - ") # TODO test/fix
-        data['variant_options'] = [] # TODO
+        item['variant_title'] = (product.attribute_value_ids.map {|i| "#{i.name}"}).join(" - ") # TODO test/fix
+        item['variant_options'] = [] # TODO
       end
-      req.session['cart'] ||= {
-        token: "TODO",
-        note: "",
-        attributes: {},
-        original_total_price: 42,
-        total_price: 42,
-        total_discount: 0,
-        total_weight: 0,
-        items: []
-      }
-      data.stringify_keys!
-      req.session['cart'].stringify_keys!
-      req.session['cart']['items'] << data # TODO deal with same product
-      req.session['cart']['item_count'] = req.session['cart']['items'].size
-      req.session['cart']['total_price'] = req.session['cart']['items'].map{|item| item['price']}.inject(0){|sum, x| sum + x }
-#      req.session['cart']['total_discount'] = req.session['cart']['items'].size
-#      req.session['cart']['total_weight'] = req.session['cart']['items'].size
+    item.stringify_keys!
+  end
 
-      @next_response = [200, { 'Content-Type' => 'application/json' }, [JSON.generate(data)]]
+  def call(env)
+    public_session = env['ooor']['ooor_public_session']
+    session = env['ooor']['ooor_session']
+    req = Rack::Request.new(env)
+    if env['PATH_INFO'] == "/cart/add.js"
+      product_id = req.params['id']
+      qty = req.params['quantity'].to_f
+      p session.public_controller_method('/shop/cart/update/', {product_id: product_id, add_qty: qty})
+      product = session.const_get('product.product').find(product_id)
+      @next_response = [200, { 'Content-Type' => 'application/json' }, [JSON.generate(item_to_cartjs(product, qty))]]
     elsif env['PATH_INFO'] == "/cart.js"
-      session = env['ooor']['ooor_public_session'] || env['ooor']['ooor_session']
-      req = Rack::Request.new(env)
-      product = session.const_get('product.product').find(req.params['id'])
-
-      data = { # TODO remove
-        token: "TODO",
-        note: "TODO",
-        attributes: {},
-        original_total_price: 42,
-        total_price: 42,
-        total_discount: 0,
-        total_weight: 0,
-        item_count: 12, # TODO
-        items: req.session['cart']['items'],
-        item_count: req.session['cart']['items'].size
-      }
-
-      @next_response = [200, { 'Content-Type' => 'application/json' }, [JSON.generate(req.session['cart'])]]
-    elsif env['PATH_INFO'] == "/cart/change.js" # POST {"quantity"=>"0", "line"=>"1"}
-    elsif env['PATH_INFO'] == "/cart" # POST
-      # TODO put stuff into Odoo cart
-      session = env['ooor']['ooor_public_session'] || env['ooor']['ooor_session']
-      req = Rack::Request.new(env)
-      order_id = session.const_get('website').find(:first).sale_get_order_rpc(true, nil, false, false, {}) # NOTE quid du force_create
+      @next_response = [200, { 'Content-Type' => 'application/json' }, [JSON.generate(cart_to_cartjs(env))]]
+    elsif env['PATH_INFO'] == "/cart/change.js"
+      line = req.params['line'].to_i
+      qty = req.params['quantity'].to_f
+      order_id = session.const_get('website').find(:first).sale_get_order_rpc(true, nil, false, false, {}) # TODO
       order = session.const_get('sale.order').find(order_id)
-      p "ooooo", order_id
-      req.session['cart']['items'].each do |item|
-        p "item", item
-        line = session.const_get('sale.order.line').find(order_id: order_id, product_id: item['product_id']).first
-        p "lllline", line
-        if line
-          order.cart_update_rpc(item['product_id'], line.id, 0, item['quantity'])
-        else
-          order.cart_update_rpc(item['product_id'], nil, 0, item['quantity'])
-        end
+      product_id = order.order_line[line - 1].product_id.id
+      session.public_controller_method('/shop/cart/update/', {product_id: product_id, set_qty: qty})
+      @next_response = [200, { 'Content-Type' => 'application/json' }, [JSON.generate(cart_to_cartjs(env))]]
+    elsif env['PATH_INFO'] == "/cart" # POST
+      cart = req.session['cart'] || {}
+      (cart['items'] || []).each do |item|
+        session.public_controller_method('/shop/cart/update/', {product_id: item['product_id'], add_qty: item['quantity']})
       end
-      @next_response = [ 302, {'Location' =>"shop/checkout"}, [] ]
+#      @next_response = [ 302, {'Location' =>"shop/checkout"}, [] ]
+      @next_response = [ 302, {'Location' =>"shop/payment"}, [] ] # Cimade specific, normally previous line
+    elsif env['PATH_INFO'] == "/shop"
+      # TODO flash de confirmation
+      @next_response = [ 302, {'Location' =>"/my/home"}, [] ]
     else
       app.call(env)
     end
